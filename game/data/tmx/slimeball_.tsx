246db62055d3a093679a119c65f694b758ce27aa
pkmark <119824422+OriginalPkmark@users.noreply.github.com>
2024-04-06 13:22:16 -0400

    Add files via upload

---
+++ game/data/tmx/slimeball_.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.10.2" name="slimeball_" tilewidth="24" tileheight="24" tilecount="56" columns="8">
+ <image source="D:/games_asset_packs/dungeonSprites_ v1.5/dungeonSprites_ v1.5/slimeball_.png" width="192" height="168"/>
+</tileset>
